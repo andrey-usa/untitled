0c0f9738efa0a2940ef614bb2b847cac6606a457
Andrey Lomako <andrey.lomako.us@gmail.com>
2020-03-28 04:25:59 -0400

    first commit

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <div className="App">
+
+
+           <h1>React</h1>
+
+        </div>
+    );
+}
+
+export default Header;
