f8a7ecb5e7db45e20774b36294c64687ee5d0476
Andrey Lomako <andrey.lomako.us@gmail.com>
2020-03-28 22:18:06 -0400

    Updates

---
+++ precache-manifest.8240836878d8d4b8d01ca4f7c6bd124c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86818d95973b329f2a87a2ec937befc",
+    "url": "/untitled/index.html"
+  },
+  {
+    "revision": "711cf89c59d9bfcb1930",
+    "url": "/untitled/static/css/main.f346aaf2.chunk.css"
+  },
+  {
+    "revision": "a15ac43fd66a37c0f564",
+    "url": "/untitled/static/js/2.68f834df.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/untitled/static/js/2.68f834df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "711cf89c59d9bfcb1930",
+    "url": "/untitled/static/js/main.0044200b.chunk.js"
+  },
+  {
+    "revision": "4fca4cf63bf499c4bb67",
+    "url": "/untitled/static/js/runtime-main.8e9b75b3.js"
+  }
+]);
